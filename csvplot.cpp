5889ffd124b3c0fd32cdd9d69a7572135d03b53d
Joey <zhu1ying3@gmail.com>
2014-02-20 17:46:13 +0800

    Finished read csv file and display it.

--- csvplot.cpp
+++ csvplot.cpp
@@ -1,5 +1,8 @@
 #include "csvplot.h"
 #include "ui_CsvPlot.h"
+#include <QFile>
+#include <QTextStream>
+#include <QPainter>
 
 //constructor for CsvPlot class
 CsvPlot::CsvPlot(QWidget *parent) :
@@ -7,8 +10,52 @@ CsvPlot::CsvPlot(QWidget *parent) :
     ui(new Ui::CsvPlot)     //TODO: don't know what is ui
 {
     ui->setupUi(this);
+    xScale = 1;
+    yScale = 1;
+    loadTextFile();
 }
 
 CsvPlot::~CsvPlot(){
     delete ui;
 }
+
+void CsvPlot::loadTextFile(){
+    QFile inputFile(":/input.txt");
+    inputFile.open(QIODevice::ReadOnly);
+    QTextStream in(&inputFile);
+
+    QStringList list;
+    list.clear();
+
+    int i = 0;
+    while(!inputFile.atEnd()){
+        QString oneLine = in.readLine();
+        list = oneLine.split(",", QString::SkipEmptyParts);
+
+        if((i > 0)&&(i < INPUT_ARRAY_ROWS)){
+            DataPNum[i] = list.at(4).toInt();
+        }
+        i++;
+    }
+    inputFile.close();
+}
+
+
+void CsvPlot::paintEvent(QPaintEvent *event){
+
+    QPainter painter(this);
+    painter.setPen(QPen(Qt::blue, 2));
+
+    QRect rect(5, 5, width() - 5*2, height() - 5*40);
+    QPolygonF polyline(INPUT_ARRAY_ROWS);
+
+    for(int i = 0; i < INPUT_ARRAY_ROWS; i++){
+        polyline[i] = QPointF(rect.left() + i * xScale, rect.bottom() - DataPNum[i] * yScale);
+    }
+    painter.drawPolyline(polyline);
+    //painter.drawLine(5, height() - 5 * 40, width(), height() - 5 * 40);
+}
+
+
+
+
--- csvplot.h
+++ csvplot.h
@@ -3,11 +3,13 @@
 
 #include <QMainWindow>
 
+#define INPUT_ARRAY_ROWS 7200       // TODO: change to const, const命名规范是啥？
+
 namespace Ui {
 class CsvPlot;
 }
 
-class CsvPlot : public QMainWindow
+class CsvPlot : public QMainWindow      //直接继承Qwidget不行吗？ 02201708
 {
     Q_OBJECT
 public:
@@ -19,7 +21,12 @@ public slots:
 
 private:
     Ui::CsvPlot *ui;
-
+    void loadTextFile();
+    int DataPNum[INPUT_ARRAY_ROWS];
+    double xScale;
+    double yScale;
+protected:
+    void paintEvent(QPaintEvent *event);
 };
 
 #endif // CSVPLOT_H
