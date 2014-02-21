801f188fba2e4feaba77fb37513c30c235a62027
Joey <zhu1ying3@gmail.com>
2014-02-21 14:50:13 +0800

    Added Grid and Curves.

--- csvplot.cpp
+++ csvplot.cpp
@@ -2,6 +2,8 @@
 #include <QFile>
 #include <QTextStream>
 #include <QPainter>
+#include <QToolButton>
+#include <QStylePainter>
 
 #include "csvplot.h"
 
@@ -13,9 +15,49 @@ CsvPlot::CsvPlot(QWidget *parent) :
     setAutoFillBackground(true);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     setFocusPolicy(Qt::StrongFocus);    //TODO: what's StrongFocus?
+
+    zoomInButton = new QToolButton(this);
+    zoomInButton->setIcon(QIcon(":/images/zoomin.png"));
+    zoomInButton->adjustSize();
+    connect(zoomInButton, SIGNAL(clicked()), this, SLOT(zoomIn()));
+
+    zoomOutButton = new QToolButton(this);
+    zoomOutButton->setIcon(QIcon(":/images/zoomout.png"));
+    zoomOutButton->adjustSize();
+    connect(zoomOutButton, SIGNAL(clicked()), this, SLOT(zoomOut()));
+
+    setPlotSettings(PlotSettings());
+
     xScale = 1;
     yScale = 1;
     loadTextFile();
+}
+
+void CsvPlot::setPlotSettings(const PlotSettings &settings){
+    zoomStack.clear();
+    zoomStack.append(settings);
+    zoomStage = 0;
+    zoomInButton->hide();
+    zoomOutButton->hide();
+    //need refresh?
+    //refreshPixmap();
+}
+
+void CsvPlot::zoomIn(){
+    xScale++;
+    update();
+}
+
+void CsvPlot::zoomOut(){
+    xScale--;
+    update();
+}
+
+void CsvPlot::resizeEvent(QResizeEvent *){
+    int x = width() - (zoomInButton->width() + zoomOutButton->width() + 10);
+    zoomInButton->move(x,5);
+    zoomOutButton->move(x + zoomInButton->width() + 5, 5);
+    refreshPixmap();
 }
 
 void CsvPlot::loadTextFile(){
@@ -39,22 +81,89 @@ void CsvPlot::loadTextFile(){
     inputFile.close();
 }
 
+void CsvPlot::paintEvent(QPaintEvent */*event*/){
 
-void CsvPlot::paintEvent(QPaintEvent *event){
-
-    QPainter painter(this);
-    painter.setPen(QPen(Qt::blue, 2));
-
-    QRect rect(5, 5, width() - 5*2, height() - 5*40);
-    QPolygonF polyline(INPUT_ARRAY_ROWS);
-
-    for(int i = 0; i < INPUT_ARRAY_ROWS; i++){
-        polyline[i] = QPointF(rect.left() + i * xScale, rect.bottom() - DataPNum[i] * yScale);
-    }
-    painter.drawPolyline(polyline);
-    //painter.drawLine(5, height() - 5 * 40, width(), height() - 5 * 40);
+    QStylePainter painter(this);
+    painter.drawPixmap(0, 0, pixmap);       //TODO: must have this, painter.draw can take effect in other functions.
 }
 
+void CsvPlot::refreshPixmap(){
 
+    pixmap = QPixmap(size());
 
+    QPainter painter(&pixmap);
+    painter.initFrom(this);
+    drawGrid(&painter);
+    drawCurve(&painter);
+    update();
+}
 
+void CsvPlot::drawGrid(QPainter *painter){
+    QRect rect(Margin, Margin, width() - 2 * Margin, height() - 2 * Margin);
+    if(!rect.isValid()){
+        return;
+    }
+
+    PlotSettings settings = zoomStack[zoomStage];
+    settings.maxX = width() - Margin*2;
+    settings.maxY = height() - Margin*2;
+    QPen quiteDark = palette().dark().color().lighter();
+    QPen light = palette().light().color();
+
+    for(int i = 0; i <= settings.numXTicks; i++){
+        int x = rect.left() + (i * (rect.width() - 1) / settings.numXTicks);
+        double label = settings.minX + (i * settings.spanX() / settings.numXTicks);
+        painter->setPen(quiteDark);
+        painter->drawLine(x,rect.top(), x, rect.bottom());
+        painter->setPen(light);
+        painter->drawLine(x, rect.bottom(), x, rect.bottom() + 5);
+        painter->drawText(x - 50, rect.bottom() + 5, 100, 20,
+                          Qt::AlignHCenter | Qt::AlignTop,
+                          QString::number(label));
+    }
+    for(int j = 0; j <= settings.numYTicks; j++){
+        int y = rect.bottom() - (j * (rect.height() - 1) / settings.numYTicks);
+        double label = settings.minY + (j * settings.spanY() / settings.numYTicks);
+        painter->setPen(quiteDark);
+        painter->drawLine(rect.left(), y, rect.right(), y);
+        painter->setPen(light);
+        painter->drawLine(rect.left() - 5, y, rect.left(), y);
+        painter->drawText(rect.left() - Margin, y - 10, Margin - 5,20,
+                          Qt::AlignRight | Qt::AlignVCenter,
+                          QString::number(label));
+    }
+    // need draw another rect?
+    //painter->drawRect(rect.adjusted(0, 0, -1, -1));
+}
+
+void CsvPlot::drawCurve(QPainter *painter){
+
+        painter->setPen(QPen(Qt::yellow, 1));
+
+        QRect rect(Margin, Margin, width() - Margin*2, height() - Margin*4);
+        QPolygonF polyline(INPUT_ARRAY_ROWS);
+
+        for(int i = 0; i < INPUT_ARRAY_ROWS; i++){
+            polyline[i] = QPointF(rect.left() + i * xScale, rect.bottom() - DataPNum[i] * yScale);
+        }
+        painter->drawPolyline(polyline);
+}
+
+QSize CsvPlot::minimumSizeHint() const
+{
+    return QSize(6 * Margin, 4 * Margin);
+}
+
+QSize CsvPlot::sizeHint() const{
+    return QSize(12 * Margin, 8 * Margin);  // default size: 800*600
+}
+
+PlotSettings::PlotSettings(){
+    minX = 0.0;
+    maxX = 10.0;
+    numXTicks = 5;
+
+    minY = 0.0;
+    maxY = 10.0;
+    numYTicks = 5;
+}
--- csvplot.h
+++ csvplot.h
@@ -2,26 +2,82 @@
 #define CSVPLOT_H
 
 #include <QWidget>
+#include <QPixmap>
+
+class QToolButton;
+class PlotSettings;     //save each zoom stage setting
 
 #define INPUT_ARRAY_ROWS 7200       // TODO: change to const, const命名规范是啥？
 
 
-class CsvPlot : public QWidget      //直接继承Qwidget不行吗？ 02201708
+class CsvPlot : public QWidget
 {
     Q_OBJECT
 public:
     explicit CsvPlot(QWidget *parent = 0);
+
+    void setPlotSettings(const PlotSettings &settings);
+
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
 signals:
 
 public slots:
+    void zoomIn();
+    void zoomOut();
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void resizeEvent(QResizeEvent *event);
 
 private:
     void loadTextFile();
+    void drawGrid(QPainter *painter);
+    void drawCurve(QPainter *painter);
+    void refreshPixmap();   //TODO: what's Pixmap?
+    QToolButton *zoomInButton;
+    QToolButton *zoomOutButton;
+    QPixmap pixmap;
+
+    enum { Margin = 50 };   //no plot area
+
+    QVector<PlotSettings> zoomStack;
+    int zoomStage;
+
     int DataPNum[INPUT_ARRAY_ROWS];
+
+
     double xScale;
     double yScale;
-protected:
-    void paintEvent(QPaintEvent *event);
+
+};
+
+class PlotSettings{
+public:
+    PlotSettings();
+
+    double spanX() const { return maxX - minX; }
+    double spanY() const { return maxY - minY; }
+
+    double minX;
+    double maxX;
+    int numXTicks;
+    double minY;
+    double maxY;
+    int numYTicks;
 };
 
 #endif // CSVPLOT_H
+
+
+
+
+
+
+
+
+
+
+
+
+
